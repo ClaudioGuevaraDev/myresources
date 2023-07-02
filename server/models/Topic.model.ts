0b28ccaf43e60e1d2751aeaba3286ba552ec61e4
Claudio Guevara <claudio.guevara.dev@gmail.com>
2023-07-02 14:44:45 -0400

    Empezamos de 0. Creamos un proyecto de next.js

--- server/models/Topic.model.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from 'mongoose'
-
-const topicSchema = new Schema({
-  name: { type: String, required: true, unique: true, trim: true },
-  image: { type: String, required: false },
-  resources: [{ type: Schema.Types.ObjectId, ref: 'Resource' }]
-}, { timestamps: true, versionKey: false })
-
-export const TopicModel = model('Topic', topicSchema)
--- server/routes/index.ts
+++
@@ -1,4 +0,0 @@
-import resourcesRoutes from './resources.routes'
-import topicsRoutes from './topics.routes'
-
-export { resourcesRoutes, topicsRoutes }
